0dbe84be2c472531d9756c07792735d4b9df07c8
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-07-07 16:35:44 +0200

    Created a template todo-list app ready for integration with a backend

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// TODO: Ensure this is read from a server
+const DATA = [
+  { id: "todo-0", name: "Sleep", completed: true },
+  { id: "todo-1", name: "Wake up", completed: false },
+  { id: "todo-2", name: "Be Awesome", completed: false },
+  { id: "todo-3", name: "Repeat", completed:false}
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
